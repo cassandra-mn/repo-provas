b40211c1136fcf44e214d29a1f218ed9d57d92c3
cassandra-mn <arthenciac@gmail.com>
2022-07-21 14:07:15 -0300

    chore: feed the database

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,50 @@
-import bcrypt from 'bcrypt';
 import {prisma} from '../src/config/database.js';
 
 async function main() {
-    const hashedPassword = bcrypt.hashSync("admin", 10);
+    await prisma.term.createMany({
+        data: [
+            {number: 1}, 
+            {number: 2}, 
+            {number: 3}, 
+            {number: 4}, 
+            {number: 5}, 
+            {number: 6}
+        ],
+        skipDuplicates: true
+    });
 
-    await prisma.user.upsert({
-        where: {email: "admin@admin.com"},
-        update: {},
-        create: {
-            email: "admin@admin.com",
-            password: hashedPassword
-        }
-  });
+    await prisma.category.createMany({
+        data: [
+            {name: "Projeto"},
+            {name: "Prática"},
+            {name: "Recuperação"}
+        ],
+        skipDuplicates: true
+    });
+
+    await prisma.discipline.createMany({
+        data: [
+            {name: "HTML  e CSS", termId: 1},
+            {name: "JavaScript", termId: 2},
+            {name: "React", termId: 3},
+            {name: "Humildade", termId: 1},
+            {name: "Planejamento", termId: 2},
+            {name: "Autoconfiança", termId: 3}
+        ],
+        skipDuplicates: true
+    });
+
+    await prisma.disciplineOnTeacher.createMany({
+        data: [
+            {teacherId: 1, disciplineId: 1},
+            {teacherId: 1, disciplineId: 2},
+            {teacherId: 1, disciplineId: 3},
+            {teacherId: 2, disciplineId: 4},
+            {teacherId: 2, disciplineId: 5},
+            {teacherId: 2, disciplineId: 6}
+        ],
+        skipDuplicates: true
+    });
 }
 
 main().catch(e => {
